fa992c0002ff736abd8cd16ae6c1575f3bdd9c75
Bharath-Radhakrishnan <Bharath.Radhakrishnan16@gmail.com>
2020-02-14 12:20:34 +0530

    Level 3 : Hash Function md5 added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+var md5 = require('md5');
 
 const app = express();
 mongoose.connect('mongodb://localhost:27017/userDB', {
@@ -31,7 +31,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,({secret:process.env.SECRET,encryptedFields:['password']}));
 
 
 const User = mongoose.model('User', userSchema);
@@ -51,7 +50,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     username: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err) {
     if (!err) {
@@ -60,11 +59,14 @@ app.post("/register", function(req, res) {
   });
 });
 app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = md5(req.body.password);
+  console.log(password);
   User.findOne({
-    username: req.body.username,
+    username: username,
   }, function(err, foundUser) {
     if (foundUser) {
-      if (foundUser.password === req.body.password) {
+      if (foundUser.password === password) {
         res.render('secrets');
       }
     } else res.send('user not found')
