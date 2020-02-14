98fb6a43b96961f078f678e331590e3fae1b9a05
Bharath-Radhakrishnan <Bharath.Radhakrishnan16@gmail.com>
2020-02-14 11:46:21 +0530

    environment variables set

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -29,8 +30,8 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-const secret = 'thisisatopsecret';
-userSchema.plugin(encrypt,({secret:secret,encryptedFields:['password']}));
+
+userSchema.plugin(encrypt,({secret:process.env.SECRET,encryptedFields:['password']}));
 
 
 const User = mongoose.model('User', userSchema);
