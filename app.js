1805f6102f6cd3f7d5b7fd2d4611413894f9fd27
Bharath-Radhakrishnan <Bharath.Radhakrishnan16@gmail.com>
2020-02-14 15:59:01 +0530

    Level 4 : Hashing and Salting using bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-var md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 mongoose.connect('mongodb://localhost:27017/userDB', {
@@ -48,27 +49,39 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    username: req.body.username,
-    password: md5(req.body.password)
+  const username= req.body.username;
+  const password= req.body.password;
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      username: username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (!err) {
+        res.render('secrets');
+      }
+    });
   });
-  newUser.save(function(err) {
-    if (!err) {
-      res.render('secrets');
-    }
-  });
+
 });
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
-  console.log(password);
+  const password = req.body.password;
+  //console.log(password);
   User.findOne({
     username: username,
   }, function(err, foundUser) {
     if (foundUser) {
-      if (foundUser.password === password) {
-        res.render('secrets');
-      }
+      bcrypt.compare(password, foundUser.password, function(err, result) {
+        if (result) {
+          res.render('secrets');
+        } else {
+          res.send('password incorrect');
+        }
+
+      });
+
     } else res.send('user not found')
   });
 });
